8256a5c6e36c5751ed421dac62ecafa5819dc36c
Benjamin Obringer <benjaminobringer@Benjamins-MacBook-Pro.local>
2023-03-20 13:22:06 -0400

    Add fn. description and fix error message

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,14 @@
+/**
+ * Takes numbers from 1 to 100 and outputs them as a string, but for multiples
+ * of three it returns “Fizz” instead of the number, and for multiples of five
+ * it returns “Buzz.” For numbers that are multiples of both three and five, it
+ * returns “FizzBuzz.”
+ *
+ * @example For input [1, 3, 5, 8, 15] the output is ['1', 'Fizz', 'Buzz', '8', 'FizzBuzz']
+ */
 export function fizzbuzz(num: number) {
   if (num < 1 || num > 100) {
-    throw new Error("number must be in the range [0, 100]");
+    throw new Error("number must be in the range [1, 100]");
   }
   if (num % 3 === 0 && num % 5 === 0) {
     return "FizzBuzz";

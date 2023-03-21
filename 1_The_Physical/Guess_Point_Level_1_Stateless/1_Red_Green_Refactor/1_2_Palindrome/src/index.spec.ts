ed5ff48d1d9ed147c0a4357d96f0ae4f54483d92
Benjamin Obringer <benjaminobringer@Benjamins-MacBook-Pro.local>
2023-03-20 20:35:29 -0400

    Implement first version of Palindrome

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,23 @@
+import { isPalindrome } from "./";
 
-describe('palindrome checker', () => {
+describe("palindrome checker", () => {
+  it("outputs true for 'mom'", () => {
+    expect(isPalindrome("mom")).toBeTruthy();
+  });
 
-})
+  it("outputs false for 'bill'", () => {
+    expect(isPalindrome("bill")).toBeFalsy();
+  });
+
+  it("outputs true for 'Mom'", () => {
+    expect(isPalindrome("Mom")).toBeTruthy();
+  });
+
+  it("outputs true for 'Was It A Rat I Saw'", () => {
+    expect(isPalindrome("Was It A Rat I Saw")).toBeTruthy();
+  });
+
+  it("outputs false for 'Change is inevitable'", () => {
+    expect(isPalindrome("Change is inevitable")).toBeFalsy();
+  });
+});

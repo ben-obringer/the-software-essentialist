b666f26fc78dcf6b4c38db3dcc213d6f5da733d9
Benjamin Obringer <benjaminobringer@Benjamins-MacBook-Pro.local>
2023-03-29 11:48:13 -0400

    [Ref-test]: Add more test cases for length

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,17 +1,23 @@
 import { validatePassword } from "./";
 
 describe("password validator", () => {
-  it("returns false with errors for passwords less than 5 chars", () => {
-    expect(validatePassword("four")).toEqual({
-      valid: false,
-      errors: ["Password must be between 5 and 15 characters"],
-    });
-  });
+  test.each([["four"], [""]])(
+    `returns false with errors for passwords less than 5 chars`,
+    (password) => {
+      expect(validatePassword(password)).toEqual({
+        valid: false,
+        errors: ["Password must be between 5 and 15 characters"],
+      });
+    }
+  );
 
-  it("returns false with errors for passwords greater than 15 chars", () => {
-    expect(validatePassword("thisissixteencha")).toEqual({
-      valid: false,
-      errors: ["Password must be between 5 and 15 characters"],
-    });
-  });
+  test.each([["thisissixteencha"], ["thisissixteenchathisissixteencha"]])(
+    `returns false with errors for passwords greater than 15 chars`,
+    (password) => {
+      expect(validatePassword(password)).toEqual({
+        valid: false,
+        errors: ["Password must be between 5 and 15 characters"],
+      });
+    }
+  );
 });

357d1a8c0faa515ad04856713eb1c2e8664e2986
Benjamin Obringer <benjaminobringer@Benjamins-MacBook-Pro.local>
2023-04-02 11:07:29 -0400

    [New]: determines min is -1 for {-1, 0}

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { calcStats } from ".";
 
-describe('stats calculator', () => {
-
-
-})
+describe("stats calculator", () => {
+  it("determines min is -1 for {-1, 0}", () => {
+    const stats = calcStats([-1, 0]);
+    expect(stats.min).toBe(-1);
+  });
+});

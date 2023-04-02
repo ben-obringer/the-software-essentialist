7fe08b3afa2ab3a0682cb0195d79664c3d9734a9
Benjamin Obringer <benjaminobringer@Benjamins-MacBook-Pro.local>
2023-04-02 11:26:06 -0400

    [New]: throws an error when input is empty

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,6 +1,10 @@
 import { calcStats } from ".";
 
 describe("stats calculator", () => {
+  it("throws an error when input is empty", () => {
+    expect(() => calcStats([])).toThrowError("input must not be empty");
+  });
+
   it("determines min is -1 for [-1, 0]", () => {
     const stats = calcStats([-1, 0]);
     expect(stats.min).toBe(-1);

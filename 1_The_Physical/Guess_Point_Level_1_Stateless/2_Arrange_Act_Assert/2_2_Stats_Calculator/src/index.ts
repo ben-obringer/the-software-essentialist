988294cee6d73344704e6e8b8707a4c635083589
Benjamin Obringer <benjaminobringer@Benjamins-MacBook-Pro.local>
2023-04-02 11:27:08 -0400

    [Ref]: Change notation sequence -> array since the input isn't technically a sequence

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -2,13 +2,13 @@ interface Stats {
   min: number;
 }
 
-export function calcStats(seq: number[]): Stats {
-  if (seq.length === 0) {
+export function calcStats(arr: number[]): Stats {
+  if (arr.length === 0) {
     throw new Error("input must not be empty");
   }
 
-  let min = seq[0];
-  for (const int of seq) {
+  let min = arr[0];
+  for (const int of arr) {
     if (int < min) {
       min = int;
     }

82d5d01964ccb91d18c24dd32c1038e1b0157913
Benjamin Obringer <benjaminobringer@Benjamins-MacBook-Pro.local>
2023-04-04 13:55:13 -0400

    [Ref]: Improve test coverage by thinking of all posible integer combinations

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -5,13 +5,43 @@ describe("stats calculator", () => {
     expect(() => calcStats([])).toThrowError("input must not be empty");
   });
 
-  it("determines min is -1 for [-1, 0]", () => {
+  it("determines min is 1 for sequences that only contain one number like [1]", () => {
+    const stats = calcStats([1]);
+    expect(stats.min).toBe(1);
+  });
+
+  it("determines min is 0 for sequences that only contain zero", () => {
+    const stats = calcStats([0]);
+    expect(stats.min).toBe(0);
+  });
+
+  it("determines min is -1 for sequence of negative numbers like [-2, -1]", () => {
+    const stats = calcStats([-2, -1]);
+    expect(stats.min).toBe(-2);
+  });
+
+  it("determines min is 1 for sequence of positive numbers like [1, 2]", () => {
+    const stats = calcStats([1, 2]);
+    expect(stats.min).toBe(1);
+  });
+
+  it("determines min is -1 for sequences with positive and negative numbers [-1, 1]", () => {
+    const stats = calcStats([-1, 1]);
+    expect(stats.min).toBe(-1);
+  });
+
+  it("determines min is -1 for sequences with zero and negative numbers like [-1, 0]", () => {
     const stats = calcStats([-1, 0]);
     expect(stats.min).toBe(-1);
   });
 
-  it("determines min is -1 for [0, -1]", () => {
-    const stats = calcStats([0, -1]);
+  it("determines min is 0 for sequences with zero and positive numbers like [0,1]", () => {
+    const stats = calcStats([0, 1]);
+    expect(stats.min).toBe(0);
+  });
+
+  it("determines min is 0 for sequences with negative numbers, zero and positive numbers like [-1, 0, 1]", () => {
+    const stats = calcStats([-1, 0, 1]);
     expect(stats.min).toBe(-1);
   });
 });

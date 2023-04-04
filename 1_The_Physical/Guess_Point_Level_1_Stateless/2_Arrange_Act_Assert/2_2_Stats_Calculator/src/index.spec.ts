1ee6fc0dfa7183721fb452da6c6f702103c0a166
Benjamin Obringer <benjaminobringer@gmail.com>
2023-04-04 14:18:30 -0400

    [Ref-test]: Simplify arrDesc

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -6,28 +6,28 @@ describe("stats calculator", () => {
   });
 
   test.each([
-    { arr: [0, 0], expected: 0, arrDesc: "of zeros" },
-    { arr: [-2, -1], expected: -2, arrDesc: "of negative numbers" },
-    { arr: [1, 2], expected: 1, arrDesc: "of positive numbers" },
+    { arr: [0, 0], expected: 0, arrDesc: "zeros" },
+    { arr: [-2, -1], expected: -2, arrDesc: "negative numbers" },
+    { arr: [1, 2], expected: 1, arrDesc: "positive numbers" },
     {
       arr: [-1, 1],
       expected: -1,
-      arrDesc: "with positive and negative numbers",
+      arrDesc: "positive and negative numbers",
     },
-    { arr: [-1, 0], expected: -1, arrDesc: "with zero and negative numbers" },
-    { arr: [0, 1], expected: 0, arrDesc: "with zero and positive numbers" },
+    { arr: [-1, 0], expected: -1, arrDesc: "zero and negative numbers" },
+    { arr: [0, 1], expected: 0, arrDesc: "zero and positive numbers" },
     {
       arr: [-1, 0, 1],
       expected: -1,
-      arrDesc: "with negative numbers, zero and positive numbers",
+      arrDesc: "negative numbers, zero and positive numbers",
     },
     {
       arr: [1],
       expected: 1,
-      arrDesc: "with only one number",
+      arrDesc: "only one number",
     },
   ])(
-    "determines min is $expected for array $arrDesc like $arr",
+    "determines min is $expected for arrays that contain $arrDesc like $arr",
     ({ arr, expected }) => {
       const stats = calcStats(arr);
       expect(stats.min).toBe(expected);
@@ -35,28 +35,28 @@ describe("stats calculator", () => {
   );
 
   test.each([
-    { arr: [0, 0], expected: 0, arrDesc: "of zeros" },
-    { arr: [-2, -1], expected: -1, arrDesc: "of negative numbers" },
-    { arr: [1, 2], expected: 2, arrDesc: "of positive numbers" },
+    { arr: [0, 0], expected: 0, arrDesc: "zeros" },
+    { arr: [-2, -1], expected: -1, arrDesc: "negative numbers" },
+    { arr: [1, 2], expected: 2, arrDesc: "positive numbers" },
     {
       arr: [-1, 1],
       expected: 1,
-      arrDesc: "with positive and negative numbers",
+      arrDesc: "positive and negative numbers",
     },
-    { arr: [-1, 0], expected: 0, arrDesc: "with zero and negative numbers" },
-    { arr: [0, 1], expected: 1, arrDesc: "with zero and positive numbers" },
+    { arr: [-1, 0], expected: 0, arrDesc: "zero and negative numbers" },
+    { arr: [0, 1], expected: 1, arrDesc: "zero and positive numbers" },
     {
       arr: [-1, 0, 1],
       expected: 1,
-      arrDesc: "with negative numbers, zero and positive numbers",
+      arrDesc: "negative numbers, zero and positive numbers",
     },
     {
       arr: [1],
       expected: 1,
-      arrDesc: "with only one number",
+      arrDesc: "only one number",
     },
   ])(
-    "determines max is $expected for array $arrDesc like $arr",
+    "determines max is $expected for arrays that contain $arrDesc like $arr",
     ({ arr, expected }) => {
       const stats = calcStats(arr);
       expect(stats.max).toBe(expected);
